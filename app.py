9866f8c1ec3bd26d8af2fcda5e0b0c897780b13c
Alexander Orion Centomain <138638930+alexor7on@users.noreply.github.com>
2025-09-03 15:52:07 -0300

    Atualiza para manter apenas números

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 # Título da aplicação
-st.title("Remover Espaços da Nota Fiscal")
+st.title("Remover espaços e caracteres")
 
 # Caixa de texto para entrada do número da NF
-nf_input = st.text_input("Digite o número da NF com espaços: ")
+nf_input = st.text_input("Digite o número da NF: ")
 
-# Se o usuário digitar algo, remover espaços e mostrar o resultado
+# Se o usuário digitar algo, remover tudo que não for número
 if nf_input:
-    nf_limpa = "".join(c for c in nf_input if not c.isspace())
-    st.success(f"NF sem espaços: {nf_limpa}")
+    nf_limpa = "".join(c for c in nf_input if c.isdigit())
+    st.success(f"NF apenas com números: {nf_limpa}")
