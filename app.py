5d8021ee3fad384b4637fd7662c098d857c53269
Alexander Orion Centomain <138638930+alexor7on@users.noreply.github.com>
2025-09-04 09:58:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 # Título da aplicação
-st.title("Remover espaços e caracteres")
+st.title("Remover Espaços e Caracteres Especiais")
 
 # Caixa de texto para entrada do número da NF
-nf_input = st.text_input("Digite um número para remover espaços e caracteres especiais: ")
+nf_input = st.text_input("Digite um número para remover os espaços e caracteres especiais: ")
 
 # Se o usuário digitar algo, remover tudo que não for número
 if nf_input:
     nf_limpa = "".join(c for c in nf_input if c.isdigit())
-    st.success(f"Segue o número: {nf_limpa}")
+    st.success(f"Número corrigido: {nf_limpa}")
